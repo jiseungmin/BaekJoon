56ae981316af346e2f9683e780797170caa78e11
jiseungmin <sm46384638@naver.com>
2022-05-02 20:05:57 +0900

    2022.05.02

--- Greedy/3.py
+++ Greedy/3.py
@@ -1,16 +1,16 @@
-a,b = map(int,input().split())
-cnt = 0
-money = []
-for i in range(a):
-  money.append(int(input()))
-money.sort()
-money.reverse()
-
-for i in range(0,a-1):
-  if b > money[i]:
-    cnt += b // money[i]
-    b = b % money[i]
-
-print(cnt)
-
-
+# 11047번문제: 준규가 가지고 있는 동전은 총 N종류이고, 각각의 동전을 매우 많이 가지고 있다.
+# 동전을 적절히 사용해서 그 가치의 합을 K로 만들려고 한다. 
+# 이때 필요한 동전 개수의 최솟값을 구하는 프로그램을 작성하시오.
+N, K = map(int, input().split()) 
+coins = []
+for _ in range(N):
+    coins.append(int(input()))
+coins.sort(reverse=True)
+answer = 0
+for coin in coins:
+    if K >= coin:
+        answer += K // coin
+        K %= coin 
+        if K <= 0: 
+          break
+print(answer) 
