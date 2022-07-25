6f397531f9c3c234c99615d093d95dc7af458572
test <test@test.com>
2022-07-25 21:46:29 +0900

    13 question

--- Greedy/11.py
+++ Greedy/11.py
@@ -1,11 +1,14 @@
-from posixpath import split
+# 모험가 길드 문제
+n = int(input())
+people = list(map(int, input().split()))
+people.sort()
+result = 0
+count = 0
 
-n = list(input())
-n.sort(reverse=True)
+for i in people:
+    count += 1
+    if count >= i:
+        result += 1
+        count = 0
 
-for i in range()
-
-  
-
-print(n)
-
+print(result)
